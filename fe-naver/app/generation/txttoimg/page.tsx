a91ae68eee550bc226d283406d06150125d46ce6
Hanseungyeon <mjk6829@gmail.com>
2024-07-17 00:41:58 +0900

    사이드바 작업, txttoimg 작업

--- fe-naver/app/generation/txttoimg/page.tsx
+++ fe-naver/app/generation/txttoimg/page.tsx
@@ -1,5 +1,19 @@
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Unstable_Grid2";
+import {TextToImagePrompt} from "@/components/generation/txttoimg/Prompt";
+
 export default function Page(){
     return (
-        <h1>txttoimg</h1>
+        <Stack spacing={3}>
+            {/*<div>*/}
+            {/*    <Typography variant="h4">텍스트 이미지 생성</Typography>*/}
+            {/*</div>*/}
+            <Grid container spacing={3}>
+                <Grid lg={12} md={6} xs={12}>
+                    <TextToImagePrompt />
+                </Grid>
+            </Grid>
+        </Stack>
     )
 }
---
+++ fe-naver/components/generation/txttoimg/Prompt.tsx
@@ -0,0 +1,110 @@
+'use client'
+
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import Divider from "@mui/material/Divider";
+import CardContent from "@mui/material/CardContent";
+import Grid from "@mui/material/Unstable_Grid2";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import FormGroup from "@mui/material/FormGroup";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import {TextField} from "@mui/material";
+import CardActions from "@mui/material/CardActions";
+
+const checkPoint = [
+    { value : 'Moyou', label: 'Moyou'},
+    { value : 'Asian', label: 'Asian'},
+    { value : 'Japan', label: 'Japan'},
+] as const;
+
+export function TextToImagePrompt() {
+    return (
+        <form
+            onSubmit={(event)=> {
+                event.preventDefault();
+            }}
+        >
+            <Card>
+                <CardHeader subheader="Text To Image Generation" title="Text To Image" />
+                <Divider />
+                <CardContent>
+                    <Grid container spacing={3}>
+                        <Grid md={2} sm={6} xs={12}>
+                            <FormControl fullWidth>
+                                <InputLabel>Check Point Model</InputLabel>
+                                <Select label="CheckPoint" name="CheckPoint" variant="outlined">
+                                    {checkPoint.map((item) => (
+                                        <MenuItem key={item.value} value={item.value}>
+                                            {item.label}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid md={5} sm={5} xs={12}>
+                            <TextField
+                                id="Positive"
+                                label="Positive"
+                                fullWidth
+                                multiline
+                                rows={3}
+                                defaultValue="Positive"
+                            />
+                        </Grid>
+                        <Grid md={5} sm={5} xs={12}>
+                            <TextField
+                                id="Negative"
+                                fullWidth
+                                label="Negative"
+                                multiline
+                                rows={3}
+                                defaultValue="Negative"
+                            />
+                        </Grid>
+                    </Grid>
+                </CardContent>
+                <Divider />
+                <CardContent>
+                    <Grid container spacing={3}>
+                        <Grid md={2} sm={6} xs={12}>
+                            <FormControl fullWidth>
+                                <InputLabel>Check Point Model</InputLabel>
+                                <Select label="CheckPoint" name="CheckPoint" variant="outlined">
+                                    {checkPoint.map((item) => (
+                                        <MenuItem key={item.value} value={item.value}>
+                                            {item.label}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid md={5} sm={5} xs={12}>
+                            <TextField
+                                id="Positive"
+                                label="Positive"
+                                fullWidth
+                                multiline
+                                rows={3}
+                                defaultValue="Positive"
+                            />
+                        </Grid>
+                        <Grid md={5} sm={5} xs={12}>
+                            <TextField
+                                id="Negative"
+                                label="Negative"
+                                fullWidth
+                                multiline
+                                rows={3}
+                                defaultValue="Negative"
+                            />
+                        </Grid>
+                    </Grid>
+                </CardContent>
+            </Card>
+        </form>
+    )
+}
