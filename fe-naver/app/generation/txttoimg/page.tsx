71ceec4e0300f5a6f5311e2c1cf86c1b42185af9
Hanseungyeon <mjk6829@gmail.com>
2024-07-25 22:34:37 +0900

    axios 세팅

--- fe-naver/app/generation/txttoimg/page.tsx
+++ fe-naver/app/generation/txttoimg/page.tsx
@@ -9,6 +9,26 @@ import LoadImage from "@/components/generation/load-image";
 import IpAdaptor from "@/components/generation/ipadaptor";
 import Box from "@mui/material/Box";
 import {FormEvent} from "react";
+import axios, {Axios} from "axios";
+
+const generateImage = async (data:any) => {
+    const client: Axios = axios.create({
+        // baseURL: process.env.REACT_APP_URL,
+        baseURL: "http://localhost:8080",
+        headers: {
+            "Content-Type": "application/json",
+        }
+    })
+
+        await client.post("/api/image/text", data)
+            .then((res)=> {
+                console.log('res', res);
+            })
+            .catch((err)=> {
+                console.log(err);
+            })
+
+}
 
 export default function Page(){
     const handleSubmit = (event: FormEvent<HTMLFormElement>): void => {
@@ -18,8 +38,12 @@ export default function Page(){
             positive: data.get('Positive'),
             negative: data.get('Negative'),
             checkPoint: data.get('CheckPoint'),
-            lora: data.get('Lora'),
+            // lora: data.get('Lora'),
         })
+
+        generateImage(data);
+
+
     }
     return (
         <Stack spacing={3}>
