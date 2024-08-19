9c67fbab31ccacfd26f67bdbf52b1971629dc75b
jj2084jj <whdms3368@gmail.com>
2024-08-19 20:54:54 +0900

    feat : add lora 박스추가

--- fe-naver/components/generation/txttoimg/Lora.tsx
+++ fe-naver/components/generation/txttoimg/Lora.tsx
@@ -1,89 +1,50 @@
 import CardContent from "@mui/material/CardContent";
 import Grid from "@mui/material/Unstable_Grid2";
-import FormControl from "@mui/material/FormControl";
-import InputLabel from "@mui/material/InputLabel";
-import Select from "@mui/material/Select";
-import MenuItem from "@mui/material/MenuItem";
-import {TextField} from "@mui/material";
 import Card from "@mui/material/Card";
-import React, {useRef, useState} from "react";
+import React, { useRef, useState } from "react";
 import CardHeader from "@mui/material/CardHeader";
 import Divider from "@mui/material/Divider";
 import CardActions from "@mui/material/CardActions";
 import Button from "@mui/material/Button";
+import LoraCard from "./ui/LoraCard";
 
 const loraList = [
-    {value: 'fantasy', label: 'fantasy'},
-    {value: 'china', label: 'china'},
+  { value: "fantasy", label: "fantasy" },
+  { value: "china", label: "china" },
 ] as const;
 
-
-function LoraCard() {
-    return (
-        <CardContent>
-            <Grid md={4} sm={6} xs={12}>
-                <InputLabel>Lora</InputLabel>
-                <Select label="Lora" name="Lora" variant="outlined" fullWidth>
-                    {loraList.map((item) => (
-                        <MenuItem key={item.value} value={item.value}>
-                            {item.label}
-                        </MenuItem>
-                    ))}
-                </Select>
-            </Grid>
-        </CardContent>
-    )
-}
-
 export default function Lora() {
+  const [loraArr, setLoraArr] = useState<number[]>([]);
 
-    return (
-        <Card>
-            <CardHeader title="Add Lora" />
-            <CardActions sx={{ justifyContent: 'flex-end' }}>
-                <Button variant="contained">Add Lora</Button>
-            </CardActions>
-            <Divider />
-            <CardContent>
-                <Grid container spacing={6} wrap="wrap">
-                    <Grid md={4} sm={6} xs={12}>
-                        <InputLabel>Lora</InputLabel>
-                        <Select label="Lora" name="Lora" variant="outlined" fullWidth>
-                            {loraList.map((item)=>(
-                                <MenuItem key={item.value} value={item.value}>
-                                    {item.label}
-                                </MenuItem>
-                            ))}
-                        </Select>
-                        <TextField sx={{mt:2}} id="strength1" label="Strength" variant="outlined" fullWidth />
-                        <TextField sx={{mt:2}} id="clip1" label="Clip" variant="outlined" fullWidth />
-                    </Grid>
-                    <Grid md={4} sm={6} xs={12}>
-                        <InputLabel>Lora</InputLabel>
-                        <Select label="Lora" name="Lora" variant="outlined" fullWidth>
-                            {loraList.map((item)=>(
-                                <MenuItem key={item.value} value={item.value}>
-                                    {item.label}
-                                </MenuItem>
-                            ))}
-                        </Select>
-                        <TextField sx={{mt:2}} id="strength2" label="Strength" variant="outlined" fullWidth />
-                        <TextField sx={{mt:2}} id="clip2" label="Clip" variant="outlined" fullWidth />
-                    </Grid>
-                    <Grid md={4} sm={6} xs={12}>
-                        <InputLabel>Lora</InputLabel>
-                        <Select label="Lora" name="Lora" variant="outlined" fullWidth>
-                            {loraList.map((item)=>(
-                                <MenuItem key={item.value} value={item.value}>
-                                    {item.label}
-                                </MenuItem>
-                            ))}
-                        </Select>
-                        <TextField sx={{mt:2}} id="strength3" label="Strength" variant="outlined" fullWidth />
-                        <TextField sx={{mt:2}} id="clip3" label="Clip" variant="outlined" fullWidth />
-                    </Grid>
+  function handleLoraArr() {
+    setLoraArr([...loraArr, loraArr.length + 1]);
+  }
+  return (
+    <Card>
+      <CardActions sx={{ justifyContent: "space-between" }}>
+        <CardHeader title="Add Lora" />
+        <Button variant="contained" onClick={handleLoraArr}>
+          Add Lora
+        </Button>
+      </CardActions>
+      <Divider />
+      <CardContent>
+        <Grid container spacing={6} wrap="wrap">
+          {loraArr.length > 0 ? (
+            loraArr.map((item, index) => {
+              return (
+                <Grid xs={4}>
+                  <LoraCard key={index} />
                 </Grid>
-            </CardContent>
-        </Card>
-    )
+              );
+            })
+          ) : (
+            <Grid xs={12} textAlign="center" color="gray">
+              Lora 를 추가해주세요!
+            </Grid>
+          )}
+        </Grid>
+      </CardContent>
+    </Card>
+  );
 }
