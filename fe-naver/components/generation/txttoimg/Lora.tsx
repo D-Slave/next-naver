9c45f74017392790094e8a427e615003dee75f69
jj2084jj <whdms3368@gmail.com>
2024-08-19 21:17:59 +0900

    feat: 삭제 가안

--- fe-naver/components/generation/txttoimg/Lora.tsx
+++ fe-naver/components/generation/txttoimg/Lora.tsx
@@ -7,6 +7,7 @@ import Divider from "@mui/material/Divider";
 import CardActions from "@mui/material/CardActions";
 import Button from "@mui/material/Button";
 import LoraCard from "./ui/LoraCard";
+import { Box } from "@mui/system";
 
 const loraList = [
   { value: "fantasy", label: "fantasy" },
@@ -17,7 +18,20 @@ export default function Lora() {
   const [loraArr, setLoraArr] = useState<number[]>([]);
 
   function handleLoraArr() {
-    setLoraArr([...loraArr, loraArr.length + 1]);
+    // 기존 배열을 복사
+    const newLoraArr: number[] = [...loraArr];
+
+    // 새로운 배열을 추가하는 로직
+    if (loraArr.length === 0) {
+      newLoraArr.push(1);
+    } else {
+      // 기존 배열의 마지막 배열에 다음 숫자를 추가
+
+      newLoraArr.push(newLoraArr.length + 1);
+    }
+
+    // 새로운 배열을 상태로 설정
+    setLoraArr(newLoraArr);
   }
   return (
     <Card>
@@ -35,6 +49,24 @@ export default function Lora() {
               return (
                 <Grid xs={4}>
                   <LoraCard key={index} />
+                  <Box width={"100%"} justifyContent="flex-end" display="flex">
+                    <Button
+                      onClick={() => {
+                        const newArr = [...loraArr];
+                        newArr.splice(index, 1);
+                        setLoraArr(newArr);
+                      }}
+                      color="error"
+                      variant="contained"
+                      style={{
+                        marginTop: "8px",
+                        textAlign: "right",
+                        width: "100px",
+                      }}
+                    >
+                      삭제
+                    </Button>
+                  </Box>
                 </Grid>
               );
             })
